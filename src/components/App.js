beaa3f67c571210c393d20c381258ada2c266dbd
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2023-04-21 08:56:07 -0700

    work through component diagram and tree. Setup all components for app

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import MenuController from './MenuController';
 
 function App() {
   return (
-
+    <MenuController />
   );
 }
 
---
+++ src/components/MenuController.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class MenuController extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemsList: []
+    };
+  }
+
+  render() {
+
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }  
+}
+
+export default MenuController;
